69de021b007bccc9ee854475a21f8fbfc9c7d042
masashiKamaya <masashikamaya@gmail.com>
2021-03-10 15:22:54 +0900

    インデントをタブに変換

--- src/role/main/App.java
+++ src/role/main/App.java
@@ -1,7 +1,7 @@
 package role.main;
 
 public class App{
-    public static void main(String[] args){
-        System.out.println("sample");
-    }
+	public static void main(String[] args){
+		System.out.println("sample");
+	}
 }
